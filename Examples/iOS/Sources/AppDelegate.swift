c8b15c7d684f79556df1dbe447628ec314a61beb
Tim Oliver <me@timoliver.com.au>
2020-09-26 00:49:14 +0800

    Added Mac Catalyst support to iOS app

--- Examples/iOS/Sources/AppDelegate.swift
+++ Examples/iOS/Sources/AppDelegate.swift
@@ -6,10 +6,11 @@
 //
 
 import UIKit
-import CocoaWebP
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    public var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
