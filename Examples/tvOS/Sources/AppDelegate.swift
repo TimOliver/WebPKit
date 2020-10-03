76dc73cb9e973e49b8738d2cba3cc921a3e968d8
Tim Oliver <me@timoliver.com.au>
2020-10-03 22:15:31 +0800

    Fixed build error on Mac Catalyst

--- Examples/tvOS/Sources/AppDelegate.swift
+++ Examples/tvOS/Sources/AppDelegate.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
