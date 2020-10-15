e3824908c422ef9a1ff50be05b0e0b3cea98fb21
Tim Oliver <me@timoliver.com.au>
2020-10-15 22:49:11 +0800

    Added tvOS project

--- WebPKitExample-tvOS/ViewController.swift
+++ WebPKitExample-tvOS/ViewController.swift
@@ -1,19 +1,20 @@
 //
 //  ViewController.swift
-//  WebPKitExample-iOS
+//  WebPKitExample-tvOS
 //
-//  Created by Tim Oliver on 12/10/20.
+//  Created by Tim Oliver on 15/10/20.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Decode and set a WebP image to
         // the image view in this view controller
         imageView.image = UIImage.webpNamed("WebPKitLogo")
@@ -21,6 +22,19 @@ class ViewController: UIViewController {
         // As this is a large image, enable trilinear filtering
         // to allow better re-sampling at smaller screen sizes
         imageView.layer.minificationFilter = .trilinear
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        // Set the rounding of the background view to
+        backgroundView.layer.cornerRadius = view.bounds.height * 0.2
+
+        // Set the rounding to be squircular
+        if #available(tvOS 13.0, *) {
+            backgroundView.layer.cornerCurve = .continuous
+        }
+
     }
 }
 
