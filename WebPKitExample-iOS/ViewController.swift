60cc1b3ef6c5fb3f2d7a713a0a712943cb45590e
Tim Oliver <me@timoliver.com.au>
2020-10-15 22:15:05 +0800

    Added image to iOS sample app

--- WebPKitExample-iOS/ViewController.swift
+++ WebPKitExample-iOS/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.image = UIImage.webpNamed("WebPKitLogo")
+        imageView.layer.minificationFilter = .trilinear
     }
-
-
 }
 
