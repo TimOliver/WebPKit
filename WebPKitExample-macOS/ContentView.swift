ae1d9222c13e11a2d976f5e090ed57c2198c0453
Tim Oliver <me@timoliver.com.au>
2020-10-16 00:24:38 +0800

    Added macOS sample app

---
+++ WebPKitExample-macOS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WebPKitExample-macOS
+//
+//  Created by Tim Oliver on 16/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
