9254f1d6f60471caae2d7c2cd087fcc3098e19de
Tim Oliver <me@timoliver.com.au>
2020-10-15 22:15:16 +0800

    Made iOS sample app build for Mac Catalyst

--- WebPKitExample-iOS/AppDelegate.swift
+++ WebPKitExample-iOS/AppDelegate.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    
+    public var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
