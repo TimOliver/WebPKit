b62913856a59b796259235c80a75dd3fcb7c69db
Tim Oliver <me@timoliver.com.au>
2020-10-17 00:22:40 +0800

    Changed watchOS app to use scaling

--- WebPKitExample-watchOS/Extension/InterfaceController.swift
+++ WebPKitExample-watchOS/Extension/InterfaceController.swift
@@ -14,7 +14,13 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet weak var interfaceImage: WKInterfaceImage!
 
     override func awake(withContext context: Any?) {
-        let webpImage = UIImage.webpNamed("WebPKitLogo")
+        // Locate the file on disk
+        guard let url = Bundle.main.url(forResource: "WebPKitLogo",
+                                        withExtension: "webp")  else { return }
+
+        // Decode a copy of the image scaled to the size of the screen
+        let webpImage = UIImage(contentsOfWebPFile: url,
+                                width: contentFrame.width)
         interfaceImage.setImage(webpImage)
     }
 }
