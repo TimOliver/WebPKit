8405b9f890cb59781f4b5a28c493df736ffd69c7
Tim Oliver <me@timoliver.com.au>
2020-10-27 00:23:03 +0800

    Added bitmap info extension

--- WebPKitTests/Encoding/WebPEncodingCGBitmapInfoTests.swift
+++ WebPKitTests/Encoding/WebPEncodingCGBitmapInfoTests.swift
@@ -11,6 +11,19 @@ import WebPKit
 class WebPEncodingCGBitmapInfoTests: WebPEncodingTests {
 
     func testRGBAImages() throws {
+        XCTAssertEqual(grayscaleTransparentPNGImage.pixelFormat, .grayscaleAlpha)
+        XCTAssertEqual(opaqueJPEGImage.pixelFormat, .rgb)
+        XCTAssertEqual(opaquePNGImage.pixelFormat, .rgb)
+        XCTAssertEqual(transparentPNGImage.pixelFormat, .rgba)
+        XCTAssertEqual(indexedTransparentPNGImage.pixelFormat, .rgba)
+    }
 
+    // Test if the images alpha properties are correct
+    func testAlphaImages() {
+        XCTAssertFalse(opaquePNGImage.hasAlpha)
+        XCTAssertFalse(opaqueJPEGImage.hasAlpha)
+        XCTAssertTrue(transparentPNGImage.hasAlpha)
+        XCTAssertTrue(grayscaleTransparentPNGImage.hasAlpha)
+        XCTAssertTrue(indexedTransparentPNGImage.hasAlpha)
     }
 }
