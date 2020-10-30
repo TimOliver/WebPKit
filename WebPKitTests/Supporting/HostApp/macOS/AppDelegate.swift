4517f7a0c60f2a5c3ae3d521ba9db8389f09d09d
Tim Oliver <me@timoliver.com.au>
2020-10-30 23:54:35 +0800

    Moved in Mac unit testing target

---
+++ WebPKitTests/Supporting/HostApp/macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WebPKitTestHost
+//
+//  Created by Tim Oliver on 30/10/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
